57d4f0cb1b9f359d6f07d3546bc9234caf61cd01
wishon1 <wisdomhonest1@gmail.com>
2024-02-01 14:42:39 -0500

    add task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -5,12 +5,6 @@ from datetime import datetime
 from fabric.api import *
 
 
-#!/usr/bin/python3
-""" generates a .tgz archive from the contents of the web_static folder"""
-from datetime import datetime
-from fabric.api import local
-
-
 def do_pack():
     """ create an a .tgz achive"""
     # get the current date and time
@@ -31,21 +25,40 @@ def do_pack():
     else:
         return archive_path
 
+
 def do_deploy(archive_path):
-    """ method that distributes an archive to my web servers"""
-    if 0s.path.exists(archive_path):
+    """Deploy the archive to web servers."""
+    # Check if the archive exists
+    if os.path.exists(archive_path):
+        # Extract archive and prepare folder paths
         archive = archive_path.split('/')[1]
         tmp_obj = "/tmp/{}".format(archive)
         folder = archive.split('.')[0]
         folder_path = "/data/web_static/releases/{}/".format(folder)
 
+        # Upload the archive to the server
         put(archive_path, tmp_obj)
+
+        # Create directory to extract archive contents
         run("mkdir -p {}".format(folder_path))
-        run("tar -xzf {} -c {}".format(tmp_obj, folder_path))
+
+        # Extract archive contents to folder path
+        run("tar -xzf {} -C {}".format(tmp_obj, folder_path))
+
+        # Remove the archive from tmp directory
         run("rm {}".format(tmp_obj))
-        run("mv -f {}web_static/* {}".format(tmp_obj, folder_path))
+
+        # Move contents of extracted folder to folder path
+        run("mv -f {}* {}".format(folder_path, folder_path))
+
+        # Remove web_static directory from folder path
         run("rm -rf {}web_static".format(folder_path))
+
+        # Remove current symbolic link
         run("rm -rf /data/web_static/current")
+
+        # Create new symbolic link to current version
         run("ln -s {} /data/web_static/current".format(folder_path))
-        return True
-    return False
+
+        return True  # Deployment successful
+    return False  # Archive not found
